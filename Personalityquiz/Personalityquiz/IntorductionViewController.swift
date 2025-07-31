af216dbc39ea443aaa23d585d791775dce4a4f2b
Hemant Sardana <156403571+hemant2417@users.noreply.github.com>
2025-07-31 15:08:08 +0530

    Personality quiz uploded

---
+++ Personalityquiz/Personalityquiz/IntorductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Personalityquiz
+//
+//  Created by Hemant Sardana on 29/07/25.
+//
+
+import UIKit
+
+class IntorductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue){
+        
+    }
+
+
+}
+
